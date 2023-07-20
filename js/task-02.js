30c3742744c973fd01bd47e864df623232ba3a86
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-07-20 16:11:45 +0300

    task number two

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// variant 1
+const ulIngredients = document.querySelector('#ingredients')
+for (const element of ingredients) {
+ const separateIng = document.createElement('li')
+  separateIng.textContent = element
+  ulIngredients.appendChild(separateIng)
+}
+console.log(ulIngredients);
+
+// variant 2
+// const ulIngredients = document.querySelector('#ingredients')
+// const arrIng = ingredients.map(elem => {
+//   const separateIng = document.createElement('li')
+//   separateIng.textContent = elem
+//   return ulIngredients.appendChild(separateIng)
+// })
